a644ec8baeaa2dfce2e763eb089c948fd2931790
Benjamin Corré <benjamin.corre@systeme-u.fr>
2019-02-27 14:17:50 +0100

    fix(searchUtilsMongo): rename searchUtils to searchUtilsMongo

--- src/modules/pagination/PaginationUtilsDAO.js
+++ src/modules/pagination/PaginationUtilsDAO.js
@@ -1,66 +1,67 @@
-import { SearchUtils } from '../search/searchUtils';
-import {
-    BusinessException,
-    ErreurDO
-} from '@ugieiris/iris-common'
-
-
+import { SearchUtilsMongo } from '../search/searchUtilsMongo'
+import { BusinessException, ErreurDO } from '@ugieiris/iris-common'
 
 /**
  * Generates object to sort
- * @param {String[] | String} sorts 
+ * @param {String[] | String} sorts
  *                              sort
  * @returns Object to sort in mongodb
  */
-async function createObjectForSort (sorts) {
-    let responseSort = {}
-    if(typeof sorts === 'string'){
-        responseSort = await createObjectForSortString(sorts)
-    }else if (typeof sorts === 'object'){
-        for (let sort in sorts) {
-            responseSort = {...responseSort, ...await createObjectForSortString(sorts[sort])}
-        }
-    }else{
-        throw new BusinessException(new ErreurDO('sort','pagination.sort.type'))
+async function createObjectForSort(sorts) {
+  let responseSort = {}
+  if (typeof sorts === 'string') {
+    responseSort = await createObjectForSortString(sorts)
+  } else if (typeof sorts === 'object') {
+    for (let sort in sorts) {
+      responseSort = { ...responseSort, ...(await createObjectForSortString(sorts[sort])) }
     }
-    return responseSort
+  } else {
+    throw new BusinessException(new ErreurDO('sort', 'pagination.sort.type'))
+  }
+  return responseSort
 }
 
-async function createObjectForSortString (sortString) {
-    try {
-        await SearchUtils.checkNoInjection(sortString)
-        const tab = sortString.split(",")
-        let objectSort = {}
-        objectSort[tab[0]] = tab[1] === 'asc' ? 1 : -1
-        return objectSort   
-    } catch (error) {
-        throw error
-    }    
+async function createObjectForSortString(sortString) {
+  try {
+    await SearchUtilsMongo.checkNoInjection(sortString)
+    const tab = sortString.split(',')
+    let objectSort = {}
+    objectSort[tab[0]] = tab[1] === 'asc' ? 1 : -1
+    return objectSort
+  } catch (error) {
+    throw error
+  }
 }
 
 /**
  * generates a paged response
- * @param {String} collection 
+ * @param {String} collection
  *                      name of collection
- * @param {Object} connectionDb 
- *                      object to connect to the database 
- * @param {Object} find 
+ * @param {Object} connectionDb
+ *                      object to connect to the database
+ * @param {Object} find
  *                      object that is being researched
- * @param {Object} query 
+ * @param {Object} query
  *                      query paramater
  * @returns Object that contains response of find and a count
-*/
-async function searchInDb (collection ,connectionDb, find, query) {
-    const sortMongo = query.sort ? await createObjectForSort(query.sort) : null
-    let response = {}
+ */
+async function searchInDb(collection, connectionDb, find, query) {
+  const sortMongo = query.sort ? await createObjectForSort(query.sort) : null
+  let response = {}
 
-    response.response = await connectionDb.collection(collection).find(find).sort(sortMongo).skip(query.size * query.page).limit(query.size).toArray()
+  response.response = await connectionDb
+    .collection(collection)
+    .find(find)
+    .sort(sortMongo)
+    .skip(query.size * query.page)
+    .limit(query.size)
+    .toArray()
 
-    response.count = await connectionDb.collection(collection).countDocuments(find)
-    return response
+  response.count = await connectionDb.collection(collection).countDocuments(find)
+  return response
 }
 
 export const PaginationUtilsDAO = {
-    createObjectForSort,
-    searchInDb
+  createObjectForSort,
+  searchInDb
 }
--- src/modules/search/searchUtilsMongo.js
+++ src/modules/search/searchUtilsMongo.js
@@ -1,143 +1,136 @@
 import { TypeUtils } from '../type/typeUtils'
-import {
-	BusinessException,
-	ErreurDO
-} from '@ugieiris/iris-common'
+import { BusinessException, ErreurDO } from '@ugieiris/iris-common'
 
 /**
  * Permits searching on a string with wildcards(*)
- * @param {Object} object 
+ * @param {Object} object
  * 					JSON object that will be sent to mongo
- * @param {String} prop 
+ * @param {String} prop
  * 					name of property
- * @param {String} param 
+ * @param {String} param
  * 					the string who is searching with wildcards
  */
-async function searchStringObject (object , prop , param) {
-	try {
-		await checkNoInjection(param)
-		const startWith = RegExp(/\*$/)
-		const endWith = RegExp(/^\*/)
-		//contains
-		if( endWith.test(param) && startWith.test(param) ){
-			param = param.replace(RegExp(/\*/, 'g'),'')
-			object[prop] = {}
-			object[prop]['$regex'] = new RegExp(param, 'i')
-		}
-		//end with
-		else if(endWith.test(param)){
-			param = param.replace(RegExp(/\*/, 'g'),'')
-			object[prop] = {}
-			object[prop]['$regex'] = new RegExp(param+'$', 'i')
-		}
-		//begin with
-		else if(startWith.test(param)){
-			param = param.replace(RegExp(/\*/, 'g'),'')
-			object[prop] = {}
-			object[prop]['$regex'] = new RegExp('^' + param , 'i')
-		}else {
-			object[prop] = param
-		}
-		
-	} catch (error) {
-		throw new BusinessException(new ErreurDO(prop , 'search.'+prop+'.string'))
-	}
-
+async function searchStringObject(object, prop, param) {
+  try {
+    await checkNoInjection(param)
+    const startWith = RegExp(/\*$/)
+    const endWith = RegExp(/^\*/)
+    //contains
+    if (endWith.test(param) && startWith.test(param)) {
+      param = param.replace(RegExp(/\*/, 'g'), '')
+      object[prop] = {}
+      object[prop]['$regex'] = new RegExp(param, 'i')
+    }
+    //end with
+    else if (endWith.test(param)) {
+      param = param.replace(RegExp(/\*/, 'g'), '')
+      object[prop] = {}
+      object[prop]['$regex'] = new RegExp(param + '$', 'i')
+    }
+    //begin with
+    else if (startWith.test(param)) {
+      param = param.replace(RegExp(/\*/, 'g'), '')
+      object[prop] = {}
+      object[prop]['$regex'] = new RegExp('^' + param, 'i')
+    } else {
+      object[prop] = param
+    }
+  } catch (error) {
+    throw new BusinessException(new ErreurDO(prop, 'search.' + prop + '.string'))
+  }
 }
 
 /**
  * Add the search less than or equal for a property
- * @param {Object} object 
+ * @param {Object} object
  * 					JSON object that will be sent to mongo
- * @param {String} prop 
+ * @param {String} prop
  * 					name of property
- * @param {String} param 
+ * @param {String} param
  * 					parameter for this property
  * @param {TYPE} type
  * 					parameter's type
- * 
+ *
  */
-async function searchMax (object, prop , param , type) {
-	try{
-		await checkNoInjection(param)
-		param = await TypeUtils.defineType(type, param)
-		if(!object[prop]){
-			object[prop] = {}
-		}
-		object[prop]['$lte'] = param
-	}catch (error){
-		throw new BusinessException(new ErreurDO(prop , 'search.'+prop+'.'+error.message))
-	}
+async function searchMax(object, prop, param, type) {
+  try {
+    await checkNoInjection(param)
+    param = await TypeUtils.defineType(type, param)
+    if (!object[prop]) {
+      object[prop] = {}
+    }
+    object[prop]['$lte'] = param
+  } catch (error) {
+    throw new BusinessException(new ErreurDO(prop, 'search.' + prop + '.' + error.message))
+  }
 }
 
 /**
  * Add the search greater than or equal for a property
- * @param {Object} object 
+ * @param {Object} object
  * 					JSON object that will be sent to mongo
- * @param {String} prop 
+ * @param {String} prop
  * 					name of property
- * @param {String} param 
+ * @param {String} param
  * 					parameter for this property
  * @param {TYPE} type
  * 					parameter's type
- * 
+ *
  */
-async function searchMin (object, prop , param , type) {
-	try{
-		await checkNoInjection(param)
-		param = await TypeUtils.defineType(type, param)
-		if(!object[prop]){
-			object[prop] = {}
-		}
-		object[prop]['$gte'] = param
-	}catch (error){
-		throw new BusinessException(new ErreurDO(prop , 'search.'+prop+'.'+error.message))
-	}
-	
+async function searchMin(object, prop, param, type) {
+  try {
+    await checkNoInjection(param)
+    param = await TypeUtils.defineType(type, param)
+    if (!object[prop]) {
+      object[prop] = {}
+    }
+    object[prop]['$gte'] = param
+  } catch (error) {
+    throw new BusinessException(new ErreurDO(prop, 'search.' + prop + '.' + error.message))
+  }
 }
 
 /**
  * Add the search for a list for a property
- * @param {Object} object 
+ * @param {Object} object
  * 					JSON object that will be sent to mongo
- * @param {String} prop 
+ * @param {String} prop
  * 					name of property
- * @param {String[]} param 
+ * @param {String[]} param
  * 					parameter for this property
  * @param {TYPE} type
  * 					parameter's type
- * 
+ *
  */
-async function searchList (object , prop , param , type) {
-	try {
-		if(!object['$or'] ) {
-			object['$or'] = []
-	    }
-	    for (let index = 0; index < param.length; index++) {
-		   checkNoInjection(param)
-		   const element = await TypeUtils.defineType(type, param[index])
-		   object.$or.push({[prop]:element})
-	    }
-	} catch (error) {
-		throw new BusinessException(new ErreurDO(prop , 'search.'+prop+'.'+error.message))
-	}
-	
+async function searchList(object, prop, param, type) {
+  try {
+    if (!object['$or']) {
+      object['$or'] = []
+    }
+    for (let index = 0; index < param.length; index++) {
+      checkNoInjection(param)
+      const element = await TypeUtils.defineType(type, param[index])
+      object.$or.push({ [prop]: element })
+    }
+  } catch (error) {
+    throw new BusinessException(new ErreurDO(prop, 'search.' + prop + '.' + error.message))
+  }
 }
 /**
  * Make sure there are no brackets.
- * @param {String} param 
+ * @param {String} param
  * 					paramater check
  */
-async function checkNoInjection (param) {
-	if(RegExp(/[{}]/).test(param)){
-		throw new Error('injection')
-	}
+async function checkNoInjection(param) {
+  if (RegExp(/[{}]/).test(param)) {
+    throw new Error('injection')
+  }
 }
 
-export const SearchUtils = {
-	checkNoInjection: checkNoInjection,
-	searchList: searchList,
-	searchMin: searchMin,
-	searchMax: searchMax,
-	searchStringObject: searchStringObject
+export const SearchUtilsMongo = {
+  checkNoInjection: checkNoInjection,
+  searchList: searchList,
+  searchMin: searchMin,
+  searchMax: searchMax,
+  searchStringObject: searchStringObject
 }
