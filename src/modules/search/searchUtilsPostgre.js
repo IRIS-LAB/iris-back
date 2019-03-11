14c8f4858567784ac261189164a35d3628161ffa
Jeremy Chauvin <jeremy.chauvin@systeme-u.fr>
2019-03-11 09:15:20 +0100

    delete useless comments

--- src/modules/search/searchUtilsPostgre.js
+++ src/modules/search/searchUtilsPostgre.js
@@ -1,28 +1,25 @@
 /**
  * advanced search for postgre
- * @module search/SearchUtilsPostgre
- *
- *
- * @example
- *  import {SearchUtilsPostgre} from '@u-iris/SearchUtilsPostgre'
- *  ............................................................
- *  //use generate where
- *  function findAll(query){
- *    let where = generateWhere(query)
- *    return await Resource.findAll({ where: where })
- *  }
+ * @module SearchUtilsPostgre
  */
 
-import { Op } from 'sequelize'
+import { Op } from 'sequelize';
+
+function checkObjectExist(object, prop) {
+  if (!object[prop]) {
+    object[prop] = {};
+  }
+}
 
 /**
  * Add the search greater than or equal for a property
  * @param {String} prop - name of property
  * @param {TypeOfProp} param - parameter for this property
+ * @param {Object} objectWhere - JSON object that will be sent to sequelize
  */
 function searchMin(prop, param, objectWhere) {
-  checkObjectExist(objectWhere, prop)
-  objectWhere[prop][Op.gte] = param
+  checkObjectExist(objectWhere, prop);
+  objectWhere[prop][Op.gte] = param;
 }
 
 /**
@@ -32,22 +29,23 @@ function searchMin(prop, param, objectWhere) {
  * @param {Object} objectWhere - JSON object that will be sent to sequelize
  */
 function searchMax(prop, param, objectWhere) {
-  checkObjectExist(objectWhere, prop)
-  objectWhere[prop][Op.lte] = param
+  checkObjectExist(objectWhere, prop);
+  objectWhere[prop][Op.lte] = param;
 }
 
 /**
  * Permits searching on a string with wildcards(*)
  * @param {String} prop - name of property
  * @param {String} param - the string who is searching with wildcards
+ * @param {Object} objectWhere - JSON object that will be sent to sequelize
  */
 function searchString(prop, param, objectWhere) {
   if (param.includes('*')) {
-    param = param.replace(RegExp(/\*/, 'g'), '%')
-    checkObjectExist(objectWhere, prop)
-    objectWhere[prop][Op.iLike] = param
+    param = param.replace(RegExp(/\*/, 'g'), '%');
+    checkObjectExist(objectWhere, prop);
+    objectWhere[prop][Op.iLike] = param;
   } else {
-    objectWhere[prop] = param
+    objectWhere[prop] = param;
   }
 }
 
@@ -55,22 +53,14 @@ function searchString(prop, param, objectWhere) {
  * Add the search for a list for a property
  * @param {String} prop - name of property
  * @param {String} param - the string who is searching with wildcards
+ * @param {Object} objectWhere - JSON object that will be sent to sequelize
  */
 function searchList(prop, param, objectWhere) {
   if (typeof param === 'string') {
-    searchString(prop, param, objectWhere)
+    searchString(prop, param, objectWhere);
   } else if (typeof param === 'object') {
-    checkObjectExist(objectWhere, prop)
-    objectWhere[prop][Op.or] = []
-    for (const key of param) {
-      objectWhere[prop][Op.or].push(key)
-    }
-  }
-}
-
-function checkObjectExist(object, prop) {
-  if (!object[prop]) {
-    object[prop] = {}
+    checkObjectExist(objectWhere, prop);
+    objectWhere[prop][Op.or] = param;
   }
 }
 
@@ -81,47 +71,45 @@ function checkObjectExist(object, prop) {
  * @returns {Object} all query params without size , sort and page
  */
 function cloneQueryWitoutSizeSortPage(query) {
-  const { size, sort, page, ...clone } = query
-  return clone
+  const {
+ size, sort, page, ...clone 
+} = query;
+  return clone;
 }
 
 function conditionForWhere(where, object, param) {
   if (param.includes('min') || param.includes('after')) {
-    const prop = param.replace(RegExp(/^(min|after)+/, 'g'), '')
-    searchMin(prop, object[param], where)
+    const prop = param.replace(RegExp(/^(min|after)+/, 'g'), '');
+    searchMin(prop, object[param], where);
   } else if (param.includes('max') || param.includes('before')) {
-    const prop = param.replace(RegExp(/^(max|before)+/, 'g'), '')
-    searchMax(prop, object[param], where)
+    const prop = param.replace(RegExp(/^(max|before)+/, 'g'), '');
+    searchMax(prop, object[param], where);
   } else {
-    searchList(param, object[param], where)
+    searchList(param, object[param], where);
   }
 }
 
 /**
- *  Generate a where who will send a sequelize
+ * @description Generate a where who will send a sequelize
  * @param {Object} query - all query params (req.query)
  * @returns {Object} object where who will send a sequelize
  *
- * @example
- *  // query = {title: 'd*', page: 0}
- * const where = await SearchUtilsPostgre.generateWhere(query)
- * // where = {title: [Op.iLike]: d% }
  */
 function generateWhere(query) {
-  const clone = cloneQueryWitoutSizeSortPage(query)
-  let where = {}
+  const clone = cloneQueryWitoutSizeSortPage(query);
+  const where = {};
   for (const key in clone) {
     if (clone.hasOwnProperty(key)) {
-      conditionForWhere(where, clone, key)
+      conditionForWhere(where, clone, key);
     }
   }
-  return where
+  return where;
 }
 
-export const SearchUtilsPostgre = {
+export default {
   searchMin,
   searchMax,
   searchString,
   searchList,
-  generateWhere
-}
+  generateWhere,
+};
