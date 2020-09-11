049cac855851e3a934c188747b599cec8dceec72
Leo Lienard <leo.lienard@systeme-u.fr>
2020-09-11 09:56:11 +0200

    feat(bootstrap): add hostname option

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -3,10 +3,11 @@ import { NestFactory } from '@nestjs/core'
 import { ExceptionFilter } from './filters'
 import { getLogger, LoggerService, setApplicationContext } from './modules/iris-module'
 
-export async function bootstrapIrisApp(appModule: Type<NestModule>, options?: { port?: number, logger?: NestApplicationOptions['logger'] }): Promise<INestApplication> {
+export async function bootstrapIrisApp(appModule: Type<NestModule>, options?: { port?: number, hostname?: string, logger?: NestApplicationOptions['logger'] }): Promise<INestApplication> {
   return new Promise(async (resolve, reject) => {
     try {
       const port = (options && options.port) || process.env.NODE_REQUESTPORT || 3000
+      const hostname = (options && options.hostname) || process.env.NODE_REQUESTHOST || 'localhost'
       const app = await NestFactory.create(appModule, {
         logger: options && options.logger !== undefined ? options.logger : (process.env.NODE_ENV === 'production' ? false : console),
       })
@@ -14,7 +15,7 @@ export async function bootstrapIrisApp(appModule: Type<NestModule>, options?: { 
 
       app.useGlobalFilters(new ExceptionFilter())
       app.useLogger(app.get(LoggerService))
-      app.listen(port, () => {
+      app.listen(port, hostname, () => {
         resolve(app)
       })
     } catch (e) {
